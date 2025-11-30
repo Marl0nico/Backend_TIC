23700542185fa749d3036f4a77f6ac1f0cccb784
Marl0nico <marlon.nicolalde@epn.edu.ec>
2025-11-29 20:28:24 -0500

    Adición del enlace de producción del Front

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use("/api/comentarios", comentariosRoutes);
 
 dotenv.config(); // Configurations
 app.set("port", process.env.PORT || 3000);
-const allowedOrigins = [process.env.URL_FRONTEND, 'http://localhost:5173', 'https://uconnect-api-s755.onrender.com'];
+const allowedOrigins = [process.env.URL_FRONTEND, 'http://localhost:5173', 'https://proyecto-tic25.netlify.app'];
 
 app.use(
   cors({
